41f7a67de4c277dfef4788b1cc5c7419f37bda03
Almog Shoob <almogshoob1@gmail.com>
2022-06-08 11:16:37 +0300

    update

--- Iterative-1d-gm.py
+++ Iterative-1d-gm.py
@@ -1,10 +1,68 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env python
+import math
 import sys
+import numpy as np
+import scipy
+from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
+
+
+def plot_smooth(x1, x2, x1g, x2g):
+    plt.figure(1)
+    plt.subplot(221)
+    plt.plot(x1)
+    plt.subplot(222)
+    plt.plot(x2, 'k', label='original x2')
+    plt.subplot(223)
+    plt.plot(x1g, 'k', label='x1 smooth')
+    plt.subplot(224)
+    plt.plot(x2g, 'k', label='x2 smooth')
+    plt.show()
+
 
 def main(argv):
-    f1, f2 = argv[0], argv[1]
-    pass
+    # get signals to numpy array
+    x1 = gaussian_filter1d(np.load(argv[0])['x1'], sigma=1)
+    x2 = gaussian_filter1d(np.load(argv[1])['x2'], sigma=1)
+
+    # match signals length
+    if len(x1) > len(x2):
+        x2 = x2[:len(x1)]
+    else:
+        x1 = x1[:len(x2)]
+
+    # loop
+    dx = 0
+    dx_update = 1
+    # TODO: for now we assume dx is positive
+    # while dx_update > 0.01:
+    #     # build and solve linear equation
+    #     n = len(x1) - math.ceil(dx)
+    #     # shift x2
+    #     dx_frac = dx - math.floor(dx)
+    #     x2s = x2[math.floor(dx):]  # move right arik used ndimage.shift
+    #     x2s = np.array([(x2s[i] + (x2s[i + 1] - x2s[i]) * dx_frac) for i in range(n - 1)])
+    #
+    #     a = np.array([(x1[i + 1] - x1[i - 1]) for i in range(1, n - 2)]).reshape((n - 3, 1))  # /2 removed
+    #     b = np.array([(x2s[i] - x1[i]) for i in range(1, n - 2)]).reshape((n - 3, 1))
+    #     dx_update = np.linalg.solve(a.T @ a, a.T @ b)[0][0]  # solve manualy
+    #     dx += dx_update
+    #     print(dx, '\t', dx_update)
+
+    while abs(dx_update) > 0.01:
+        # build and solve linear equation
+        n = len(x2) - math.ceil(dx)
+        # shift x1
+        dx_frac = dx - math.floor(dx)
+        x1s = x1[math.floor(dx):]  # move right arik used ndimage.shift
+        x1s = np.array([(x1s[i] + (x1s[i + 1] - x1s[i]) * (1-dx_frac)) for i in range(n - 1)])
+
+        a = np.array([(x2[i + 1] - x2[i - 1]) for i in range(1, n - 2)]).reshape((n - 3, 1))  # /2 removed
+        b = np.array([(x1s[i] - x2[i]) for i in range(1, n - 2)]).reshape((n - 3, 1))
+        dx_update = np.linalg.solve(a.T @ a, a.T @ b)[0][0]
+        dx += dx_update
+        print(dx, '\t', dx_update)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- run_prepare_hw_data.py
+++ run_prepare_hw_data.py
@@ -5,44 +5,49 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.ndimage import gaussian_filter
 
-
-#create 1D signal
-x = np.random.uniform(0,1,200)
-x = np.convolve(x, np.ones(10)/10, mode='same')
-
-plt.plot(x)
-plt.show()
+# create 1D signal
+x = np.random.uniform(0, 1, 10000)
+x = np.convolve(x, np.ones(10) / 10, mode='same')
 
 Downsample = 2
-Delta      = 1
+Delta = 1
 x1 = x[0::Downsample]
 x2 = x[Delta::Downsample]
-dx = Delta/Downsample
+dx = Delta / Downsample
+
+plt.subplot(131)
+plt.plot(x)
+plt.subplot(132)
+plt.plot(x1)
+plt.subplot(133)
+plt.plot(x2)
+plt.show()
 
 print('dx = ' + repr(dx))
 
-np.savez_compressed('data1d.npz',x=x, Downsample=Downsample, Delta=Delta, x1=x1, x2=x2)
+# np.savez_compressed('data1dx1.npz', x=x, Downsample=Downsample, Delta=Delta, x1=x1)
+np.savez_compressed('data1dx1.npz', x1=x1)
+np.savez_compressed('data1dx2.npz', x2=x2)
 
-
-
-#create 2D signal
+# create 2D signal
 im = cv2.imread('peppers.bmp')
-cv2.imshow('image', im);cv2.waitKey(0)
+cv2.imshow('image', im)
+cv2.waitKey(0)
 
 SmoothdImg = gaussian_filter(im, sigma=3)
-cv2.imshow('smoothed', SmoothdImg);cv2.waitKey(0)
+cv2.imshow('smoothed', SmoothdImg)
+cv2.waitKey(0)
 
 Downsample = 2
-DeltaX     = 3
-DeltaY     = 3
-im1 = SmoothdImg[0::Downsample,0::Downsample]
-im2 = SmoothdImg[DeltaY::Downsample,DeltaX::Downsample]
-dx = DeltaX/Downsample
-dy = DeltaY/Downsample
+DeltaX = 3
+DeltaY = 3
+im1 = SmoothdImg[0::Downsample, 0::Downsample]
+im2 = SmoothdImg[DeltaY::Downsample, DeltaX::Downsample]
+dx = DeltaX / Downsample
+dy = DeltaY / Downsample
 
-cv2.imwrite('im1.bmp',im1)
-cv2.imwrite('im2.bmp',im2)
+cv2.imwrite('im1.bmp', im1)
+cv2.imwrite('im2.bmp', im2)
 
-np.savez_compressed('data2d.npz',dx=dx,dy=dy, Downsample=Downsample,
+np.savez_compressed('data2d.npz', dx=dx, dy=dy, Downsample=Downsample,
                     im1=im1, im2=im2)
-
