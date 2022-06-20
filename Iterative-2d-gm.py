3a82264e22052641527bfc16f35234eda22eaa23
Almog Shoob <almogshoob1@gmail.com>
2022-06-20 23:02:36 +0300

    FINISH

--- Iterative-2d-gm.py
+++ Iterative-2d-gm.py
@@ -14,22 +14,22 @@ def interpolation_2d(x1, x2, dx, dy):
     x1si = x1.copy()
     x2si = x2.copy()
     # x direction
-    if dx < 0:  # shift x2 left
-        x2si = x2si[math.floor(dx_abs):, :]  # shift left
-        x1si = x1si[:-math.floor(dx_abs), :] if math.floor(dx_abs) > 0 else x1si  # cut to the overlap area
+    if dx > 0:  # shift x2 left
+        x2si = x2si[:, math.floor(dx_abs):]  # shift left
+        x1si = x1si[:, :-math.floor(dx_abs)] if math.floor(dx_abs) > 0 else x1si  # cut to the overlap area
         dir_x = 1  # for the interpolation
     else:  # shift x1 left
-        x1si = x1si[math.floor(dx):, :]  # shift left
-        x2si = x2si[:-math.floor(dx_abs), :] if math.floor(dx_abs) > 0 else x2si  # cut to the overlap area
+        x1si = x1si[:, math.floor(dx_abs):]  # shift left
+        x2si = x2si[:, :-math.floor(dx_abs)] if math.floor(dx_abs) > 0 else x2si  # cut to the overlap area
         dir_x = -1  # for the interpolation
     # y direction
-    if dy < 0:  # shift x2 up
-        x2si = x2si[:, math.floor(dy_abs):]  # shift up
-        x1si = x1si[:, :-math.floor(dy_abs)] if math.floor(dy_abs) > 0 else x1si  # cut to the overlap area
+    if dy > 0:  # shift x2 up
+        x2si = x2si[math.floor(dy_abs):, :]  # shift up
+        x1si = x1si[:-math.floor(dy_abs), :] if math.floor(dy_abs) > 0 else x1si  # cut to the overlap area
         dir_y = 1  # for the interpolation
     else:  # shift x1 up
-        x1si = x1si[:, math.floor(dy):]  # shift up
-        x2si = x2si[:, :-math.floor(dy_abs)] if math.floor(dy_abs) > 0 else x2si  # cut to the overlap area
+        x1si = x1si[math.floor(dy_abs):, :]  # shift up
+        x2si = x2si[:-math.floor(dy_abs), :] if math.floor(dy_abs) > 0 else x2si  # cut to the overlap area
         dir_y = -1  # for the interpolation
 
     # shift x2 by fraction
@@ -39,8 +39,8 @@ def interpolation_2d(x1, x2, dx, dy):
     # interpolation equation from class
     for i in range(1, x1si.shape[0] - 1):
         for j in range(1, x1si.shape[1] - 1):
-            x2s[i, j] = x2si[i, j] * (1 - alpha) * (1 - beta) + x2si[i + dir_x, j] * alpha * (1 - beta) + \
-                        x2si[i, j + dir_y] * (1 - alpha) * beta + x2si[i + dir_x, j + dir_y] * alpha * beta
+            x2s[i, j] = x2si[i, j] * (1 - alpha) * (1 - beta) + x2si[i + dir_y, j] * (1 - alpha) * beta + \
+                        x2si[i, j + dir_x] * alpha * (1 - beta) + x2si[i + dir_y, j + dir_x] * alpha * beta
     x2s = x2s[1:-1, 1:-1]  # cut out the frame (the equation doesn't valid there)
     x1si = x1si[1:-1, 1:-1]  # cut to the same shape
 
@@ -78,8 +78,9 @@ def main(argv):
     # start iterative algorithm
     dx, dy = 0, 0
     threshold = 0.0002
+    timeout = 20  # NOTE: can be increased if needed
     dx_update, dy_update = threshold, threshold
-    while np.abs(dx_update) + np.abs(dy_update) >= threshold:
+    while np.abs(dx_update) + np.abs(dy_update) >= threshold and timeout > 0:
 
         # shift images and keep only overlap area
         x1o, x2o = interpolation_2d(x1, x2, dx, dy)  # 'o' for 'overlap'
@@ -89,9 +90,9 @@ def main(argv):
         for i in range(1, x1o.shape[0] - 1):
             for j in range(1, x1o.shape[1] - 1):
                 # calculations
-                der_x = int(x1o[i + 1, j]) - int(x1o[i - 1, j])
-                der_y = int(x1o[i, j + 1]) - int(x1o[i, j - 1])
-                b_val = int(x2o[i, j]) - int(x1o[i, j])
+                der_x = int(x2o[i, j + 1]) - int(x2o[i, j - 1])
+                der_y = int(x2o[i + 1, j]) - int(x2o[i - 1, j])
+                b_val = int(x1o[i, j]) - int(x2o[i, j])
                 # add row (=equation)
                 a = np.append(a, [[der_x, der_y]], axis=0)
                 b = np.append(b, [[b_val]], axis=0)
@@ -102,6 +103,7 @@ def main(argv):
         dx_update, dy_update = sol[0, 0], sol[1, 0]
         dx += dx_update
         dy += dy_update
+        timeout -= 1
 
     print('dx = %.2f' % dx, ', dy = %.2f' % dy)
 
